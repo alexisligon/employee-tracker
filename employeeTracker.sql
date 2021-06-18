0434a10a5feb84dc6221af6516ca533d2e26f201
alexisligon <aligo@macbook-pro.home>
2021-06-18 10:58:53 -0400

    created employee table

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -17,3 +17,12 @@ department_id INT,
 PRIMARY KEY (id)
 );
 
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name DECIMAL(6),
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
+);
+
